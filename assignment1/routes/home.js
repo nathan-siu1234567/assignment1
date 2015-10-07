86a4a28fc6e0da385f1b3fb7fb2ee4a501e39376
nathan-siu <nathansiu9730@gmail.com>
2015-10-07 10:55:57 -0400

    Revert "first commit"
    This reverts commit 1e32c9a582921629c1618a15b39e4afa11da8d57.

--- assignment1/routes/home.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home', message: 'welcome to my portfolio' });
-});
-
-module.exports = router;
